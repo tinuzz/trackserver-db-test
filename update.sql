690f9afec053a412377a0c4cd5427ccdfafcc46c
Martijn Grendelman <m@rtijn.net>
2019-09-04 23:34:17 +0200

    Add 'create index' for multi-column index

--- update.sql
+++ update.sql
@@ -5,3 +5,5 @@ ALTER TABLE `wp_ts_tracks` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unic
 ALTER TABLE `wp_ts_tracks` CHANGE `name` `name` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
 ALTER TABLE `wp_ts_tracks` CHANGE `source` `source` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
 ALTER TABLE `wp_ts_tracks` CHANGE `comment` `comment` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
+
+CREATE INDEX trip_id_occurred ON wp_ts_locations (trip_id,occurred);

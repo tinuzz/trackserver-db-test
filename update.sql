bcb451037ed7d4ad38ab8b399c1f8936863ecfb5
Martijn Grendelman <m@rtijn.net>
2019-09-04 23:14:51 +0200

    Add ALTER TABLE statements to update the default timestamp values

--- update.sql
+++ update.sql
@@ -1,3 +1,6 @@
+ALTER TABLE `wp_ts_locations` ALTER occurred SET DEFAULT '1971-01-01 00:00:00', ALTER created SET DEFAULT '1971-01-01 00:00:00';
+ALTER TABLE `wp_ts_tracks` ALTER created SET DEFAULT '1971-01-01 00:00:00';
+
 ALTER TABLE `wp_ts_tracks` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE `wp_ts_tracks` CHANGE `name` `name` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
 ALTER TABLE `wp_ts_tracks` CHANGE `source` `source` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
